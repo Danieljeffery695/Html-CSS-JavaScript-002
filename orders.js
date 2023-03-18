2c9c246787ee2ab5a8129cec7e8b9d9613923b0f
Danieljeffery695 <120072127+Danieljeffery695@users.noreply.github.com>
2023-03-18 15:07:37 -0700

    three major content

---
+++ orders.js
@@ -0,0 +1,35 @@
+const Order = [
+    {
+        productName: "Foldable Mini Drone",
+        ProductNumber: "84793",
+        paymentStatus: "Due",
+        shipping: "Declined"
+    },
+    {
+        productName: "LAVENDER KF102 Drone",
+        ProductNumber: "494749",
+        paymentStatus: "Refunded",
+        shipping: "Declined"
+    },
+    {
+        productName: "Ruko Fill Pro Drone",
+        ProductNumber: "83748",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Drone with Camera Drone",
+        ProductNumber: "494749",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "GPS 4k Drone",
+        ProductNumber: "494739",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        
+    }
+]
